7053c2d5dd800bc40a461e37fb00f1b77cc71499
santuan <smrcouto88@gmail.com>
2025-09-25 23:52:20 -0300

    cambio mi libreria

--- src/modules/MyLibrary.vue
+++ src/modules/MyLibrary.vue
@@ -2,7 +2,7 @@
   <div class="max-h-screen overflow-y-auto overflow-x-hidden">
     <UContainer class="pt-24 grid gap-3">
       <h1 class="text-2xl">Lista de reproducción</h1>
-      <h2 class="text-7xl">Tus episodios</h2>
+      <h2 class="text-7xl">Mi librería</h2>
       <div class="h-4 w-96 bg-gray-500 mt-6"></div>
     </UContainer>
     <UContainer class="grid gap-2 grid-cols-6 mt-6" v-for="i in 5" :key="i">
