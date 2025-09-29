aa23bf1bf2a7cd51560ee25caffceca5bc49d989
santuan <smrcouto88@gmail.com>
2025-09-29 01:18:10 -0300

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
 import './main.css'
-
 import { createApp } from 'vue'
-
 import { createPinia } from 'pinia'
 import ui from '@nuxt/ui/vue-plugin'
 import App from './App.vue'
 import router from './router'
 import SpatialNavigation from 'js-spatial-navigation'
-
 window.SpatialNavigation = SpatialNavigation
-
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
-
 app.use(ui)
 app.mount('#app')
